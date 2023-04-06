ec41628869f0045b1388977bcedb390d979b7617
tripleAVB <venturebuildertriplea@gmail.com>
2023-04-06 20:21:59 -0300

    Implementando 2fa

--- src/2fa/sms/sms.controller.ts
+++ src/2fa/sms/sms.controller.ts
@@ -1,14 +1,35 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
 import { SmsService } from "./sms.service";
+import { Sms } from "./sms";
 
 @Controller()
 export class SmsController {
-    constructor(private readonly smsService: SmsService) {}
+  private pendingCodes = {};
+ 
 
-    @Post('envio')
-    async sendSms(@Body() data){
-        const response = await this.smsService.sendSms(data);
-        console.log(response.data)
-    }
+  constructor(private readonly smsService: SmsService) {}
+
+  @Post('send-email')
+  async sendVerificationCode(@Body() data: Sms): Promise<string> {
+    const phoneNumber = data.numero[0];
+    const verificationCode = await this.smsService.sendSms(data);
+    this.pendingCodes[phoneNumber] = verificationCode;
+    return 'Código de verificação enviado com sucesso.';
+  }
+
+  @Post('validar-codigo')
+async validateVerificationCode(@Body() payload: { phoneNumber: string, verificationCode: string }): Promise<{ valid: boolean }> {
+  const { phoneNumber, verificationCode } = payload;
+  const pendingCode = this.pendingCodes[phoneNumber];
+  if (pendingCode === verificationCode) {
+    // Código de verificação válido, limpar pendingCodes e retornar verdadeiro
+    delete this.pendingCodes[phoneNumber];
+    return { valid: true };
+  } else {
+    // Código de verificação inválido, retornar falso
+    return { valid: false };
+  }
 }
 
+
+}
--- src/2fa/sms/sms.service.ts
+++ src/2fa/sms/sms.service.ts
@@ -4,12 +4,42 @@ import { Sms } from "./sms";
 
 @Injectable()
 export class SmsService {
-    async sendSms(data: Sms){
-        try {
-            return await axios.post('https://app.websms.com.br/sms/shortcode/routes/sms.php', data);
-        } catch (error) {
-            throw new Error(`Failed to send SMS: ${error.message}`);
-        }
+
+    private pendingCodes: { [phoneNumber: string]: string } = {};
+
+  private generateVerificationCode(): string {
+    const randomNumber = Math.floor(Math.random() * 100000);
+    return randomNumber.toString().padStart(5, '0');
+  }
+
+  async sendSms(data: Sms): Promise<string> {
+    const verificationCode = this.generateVerificationCode();
+    const mensagem = `Seu código de verificação é ${verificationCode}. Não compartilhe com ninguém.`;
+    const response = await axios.post('https://app.websms.com.br/sms/shortcode/routes/sms.php', {
+      mensagem,
+      acao: data.acao, //'enviar',
+      numero: data.numero, //'22992248416',
+      hash: data.hash// 'e32256a02b211708fdcb5b588437a6e3',
+    });
+    if (response.data === '{ status: "Enviado" }') {
+      return verificationCode;
+    } else {
+      throw new Error(`Failed to send SMS: ${response}`);
     }
-    
+  }
+
+  async validateVerificationCode(phoneNumber: string, verificationCode: string): Promise<boolean> {
+    const pendingCode = this.pendingCodes[phoneNumber];
+    if (pendingCode === verificationCode) {
+      // Código de verificação válido, limpar pendingCodes e retornar verdadeiro
+      delete this.pendingCodes[phoneNumber];
+      return true;
+    } else {
+      // Código de verificação inválido, retornar falso
+      return false;
+    }
+  }
+
 }
+
+//
